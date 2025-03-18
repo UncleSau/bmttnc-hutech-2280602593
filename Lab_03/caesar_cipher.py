f7c22a0b87075e5b5b1e78a3803f1b7c897f8c08
UncleSau <akatsukipain1st@gmail.com>
2025-03-18 16:19:01 +0700

    Add files via upload

--- Lab_03/caesar_cipher.py
+++ Lab_03/caesar_cipher.py
@@ -12,17 +12,17 @@ class MyApp(QMainWindow):
         self.ui.btn_decrypt.clicked.connect(self.call_api_decrypt)
 
     def call_api_encrypt(self):
-        url = "http://127.0.0.1:5000/api/caesar/encrypt"
+        url = "http://127.0.0.1:5000/api/caesar/encrypt"  # Định nghĩa URL trước khi sử dụng
         payload = {
             "plain_text": self.ui.txt_plain_text.toPlainText(),
-            "key": self.ui.txt_key.text()
+            "key": self.ui.txt_key.toPlainText()
         }
 
         try:
             response = requests.post(url, json=payload)
             if response.status_code == 200:
                 data = response.json()
-                self.ui.txt_cipher_text.setText(data["encrypted_message"])
+                self.ui.textEdit.setText(data["encrypted_message"])
 
                 msg = QMessageBox()
                 msg.setIcon(QMessageBox.Information)
@@ -31,14 +31,15 @@ class MyApp(QMainWindow):
             else:
                 print("Error while calling API")
         except requests.exceptions.RequestException as e:
-            print("Error: %s" % e.message)
+            print("Error:", str(e))
 
     def call_api_decrypt(self):
-        url = "http://127.0.0.1:5000/api/caesar/decrypt"
+        url = "http://127.0.0.1:5000/api/caesar/decrypt"  # Định nghĩa URL trước khi sử dụng
         payload = {
-            "cipher_text": self.ui.txt_cipher_text.toPlainText(),
-            "key": self.ui.txt_key.text()
+            "cipher_text": self.ui.textEdit.toPlainText(),
+            "key": self.ui.txt_key.toPlainText()
         }
+
         try:
             response = requests.post(url, json=payload)
             if response.status_code == 200:
@@ -52,7 +53,7 @@ class MyApp(QMainWindow):
             else:
                 print("Error while calling API")
         except requests.exceptions.RequestException as e:
-            print("Error: %s" % e.message)
+            print("Error:", str(e))
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
